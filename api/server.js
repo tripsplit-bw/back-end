eca3428a65d0f8d24a71cbaa4f9145ef4e779aa6
gspayonk <grissobel.castro@gmail.com>
2019-11-21 08:14:58 -0500

    Testing data

--- api/server.js
+++ api/server.js
@@ -19,9 +19,9 @@ server.use('/api/auth', authRouter);
 server.use('/api/profile', authRouter);
 server.use('/api/users', usersRouter);
 server.use('/api/trips', tripsRouter);
-server.use('/tripMembers', tripMemberRouter);
+server.use('api/tripMembers', tripMemberRouter);
 server.use('/api/expenses', expensesRouter);
-server.use('/expenseMember', expenseMemberRouter);
+server.use('api/expenseMember', expenseMemberRouter);
 
 server.get('/', (req, res) => {
     res.send('Server Live!');
